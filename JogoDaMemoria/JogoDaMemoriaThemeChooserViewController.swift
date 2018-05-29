77f0f5f0063840540d523cb804d0417aa74ce658
EvertonCa <eve.023@hotmail.com>
2018-05-29 06:27:41 -0300

    iPad support

--- JogoDaMemoria/JogoDaMemoriaThemeChooserViewController.swift
+++ JogoDaMemoria/JogoDaMemoriaThemeChooserViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class JogoDaMemoriaThemeChooserViewController: UIViewController
+class JogoDaMemoriaThemeChooserViewController: UIViewController, UISplitViewControllerDelegate
 {
     let theme = [
         "Sports" : "⚽️🏀🏈⚾️🎾🏐🏉🎱🏓🏸🥅🏒⛳️🏹",
         "Animals":"🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐸",
         "Faces" : "😀😆😅😂🤣😇😍😌😋😛😎🧐🤪😢"
     ]
+    
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool
+    {
+        if let jdmvc = secondaryViewController as? JogoDaMemoriaViewController
+        {
+            if jdmvc.theme == nil
+            {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBAction func changeTheme(_ sender: Any)
+    {
+        if let jdmvc = splitViewDetailJogoDaMemoriaViewController
+        {
+            if let themeName = ( sender as? UIButton )?.currentTitle, let theme = theme[themeName]
+            {
+                jdmvc.theme = theme
+            }
+        }
+        else if let jdmvc = lastSeguedToJogoDaMemoriaViewController
+        {
+            if let themeName = ( sender as? UIButton )?.currentTitle, let theme = theme[themeName]
+            {
+                jdmvc.theme = theme
+            }
+            navigationController?.pushViewController(jdmvc, animated: true)
+        }
+        else
+        {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailJogoDaMemoriaViewController: JogoDaMemoriaViewController?
+    {
+        return splitViewController?.viewControllers.last as? JogoDaMemoriaViewController
+    }
+    
+    private var lastSeguedToJogoDaMemoriaViewController: JogoDaMemoriaViewController?
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
@@ -25,6 +71,7 @@ class JogoDaMemoriaThemeChooserViewController: UIViewController
                 if let jdmvc = segue.destination as? JogoDaMemoriaViewController
                 {
                     jdmvc.theme = theme
+                    lastSeguedToJogoDaMemoriaViewController = jdmvc
                 }
             }
         }
